e482b512b66db3d2004d586d03f975d9b210f215
Administrator <lionel@buzzy.cc>
2018-12-24 18:28:55 +0800

    跳转链接

--- project/app.js
+++ project/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var transfer = require('./routes/transfer');
 var novelTransfer = require('./routes/novelTransfer');
+var index = require('./routes/index');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/transfer',transfer);
 app.use('/novel_transfer',novelTransfer);
+app.use('/',index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
